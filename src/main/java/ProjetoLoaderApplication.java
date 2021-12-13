9c4bca999ac9ce669e46980be15322883e3527bd
IsraelCavalcante58 <leopontorua@gmail.com>
2021-12-12 22:45:33 -0300

    Get HardDisk S/N

--- src/main/java/ProjetoLoaderApplication.java
+++ src/main/java/ProjetoLoaderApplication.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 
 public class ProjetoLoaderApplication {
     public static void main(String[] args) {
@@ -5,6 +6,15 @@ public class ProjetoLoaderApplication {
 //        javax.swing.JOptionPane.showConfirmDialog((java.awt.Component)
 //                        null, cpuId, "Motherboard serial number",
 //                javax.swing.JOptionPane.DEFAULT_OPTION);
+
+        String sn = HardDiskSerialNumber.getSerialNumber("C");
+        javax.swing.JOptionPane.showConfirmDialog((java.awt.Component)
+                        null, sn, "Serial Number of C:",
+                javax.swing.JOptionPane.DEFAULT_OPTION);
+        System.out.println("HD " + sn);
         System.out.println(cpuId);
     }
+
+
+
 }
